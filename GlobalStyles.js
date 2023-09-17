7c48c16191f5151028b6c066675f99381d6e0572
Ajay-droid23 <97353658+Ajay-droid23@users.noreply.github.com>
2023-09-17 23:06:41 +0530

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,23 @@
+/* fonts */
+export const FontFamily = {
+  poppinsSemiBold: "Poppins-SemiBold",
+  poppinsRegular: "Poppins-Regular",
+  poppinsMedium: "Poppins-Medium",
+};
+/* font sizes */
+export const FontSize = {
+  size_smi: 13,
+  size_mini: 15,
+};
+/* Colors */
+export const Color = {
+  colorSalmon: "#ec7669",
+  colorGray: "#0a0909",
+  colorBlack: "#000",
+  colorDarkgray: "#9b9b9b",
+  colorWhitesmoke: "#f8f8f6",
+};
+/* border radiuses */
+export const Border = {
+  br_5xl: 24,
+};
