0d85e120d55919a8b0ecf20e54156e269b6047ec
Ajay-droid23 <97353658+Ajay-droid23@users.noreply.github.com>
2023-09-17 23:04:49 +0530

    Create appNavigation.js

---
+++ navigation/appNavigation.js
@@ -0,0 +1,31 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Med from '../screens/Med'
+import Home from '../screens/Home';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Welcome'
+      screenOptions={{
+        headerShown:false,
+        animation :'simple_push',
+      }}>
+       <Stack.Screen name="Med" options={{headerShown: false}} component={Med} />
+        <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
+        <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen} />
+        <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
+        <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
